6e1855c23869872d128e04b5376330f8aabf0c18
GonzaloGerez <goonzagerez@gmail.com>
2022-11-16 20:27:14 -0300

    registrox2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
 
   {
-    path:'login',
+    path:'auth',
     loadChildren: ()=>
       import('./modules/main/main.module').then((m)=> m.MainModule)
   }
--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -3,17 +3,20 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { MaterialModule } from 'src/app/material/material.module';
 import { MainRoutingModule } from './main-routing.module';
+import { RegisterComponent } from './register/register.component';
 
 
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     CommonModule,
     MaterialModule,
-    MainRoutingModule
+    MainRoutingModule,
+    
   ]
 })
 export class MainModule { }
