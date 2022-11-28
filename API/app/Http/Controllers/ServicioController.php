cd48daabaac8ad6b1a2699fa545e44968b859ebb
Soporte <alejocorrea30071998@hotmail.com>
2022-11-28 09:43:14 -0300

    cambios

--- API/app/Http/Controllers/ServicioController.php
+++ API/app/Http/Controllers/ServicioController.php
@@ -4,207 +4,234 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Servicio;
+use App\Models\Oferta;
 use App\Models\Pago;
 use App\Models\Reserva;
+use App\Models\Vuelo;
 use App\Models\User;
 use Illuminate\Support\Facades\Validator;
 
 class ServicioController extends Controller
 {
-    public function agregarServicio(Request $req){
 
-        $validator = Validator::make($req->all(), [
-            'id_reserva' => 'required|integer',
-            'id_agencia' => 'required|integer',
-            'id_vuelo' => 'required|integer',
-            'precio' => 'required|integer',
-            'fechaInicio' => 'required|date',
-            'fechaFin' => 'required|date',
+//A partir de acá Oferta
+
+public function createOferta(Request $req){
+    $oferta = new Oferta();
+
+    $oferta->fechaFin = $req->fechaFin;
+    $oferta->fechaInicio = $req->fechaInicio;
+
+    try {
+        $oferta->save();
+    } catch (\Throwable $th) {
+        return response()->json([
+            "message" => "Error al guardar la oferta",
+            "error" => $th->getMessage()
         ]);
-
-        if($validator->fails()){
-                return response()->json($validator->errors()->toJson(),400);
-        }
-
-
-        $servicio = new Servicio();
-
-        $servicio->id_reserva = $req->id_reserva;
-        $servicio->id_agencia = $req->id_agencia;
-        $servicio->id_vuelo = $req->id_vuelo;
-        $servicio->precio = $req->precio;
-        $servicio->fechaInicio = $req->fechaInicio;
-        $servicio->fechaFin = $req->fechaFin;
-
-        try {
-            $servicio->save();
-        } catch (\Throwable $th) {
-            return response()->json([
-                "message" => "Error al agregar el servicio",
-                "error" => $th->getMessage()
-            ],503);
-        }
-
-        return response()->json("Servicio agregado con exito");
     }
 
-    public function buscarServicio(Request $req){
+    return response()->json("Oferta creada con exito");
+}
 
+public function updateOferta(Request $req){
+    try {
+        $oferta = Oferta::where('id_oferta',$req->id_oferta)->firstOrFail();
+    } catch (\Throwable $th) {
+        return response()->json([
+            "message" => "Oferta seleccionada inexistente",
+            "error" => $th->getMessage()
+        ],406);
     }
 
-    public function listarReservas($id_usuario){
+    $oferta->fechaFin = $req->fechaFin;
+    $oferta->fechaInicio = $req->fechaInicio;
 
-        $validator = Validator::make($req->all(), [
-            'id_usuario' => 'required',
+    try {
+        $oferta->save();
+    } catch (\Throwable $th) {
+        return response()->json([
+            "message" => "Error al actualizar la oferta",
+            "error" => $th->getMessage()
         ]);
-
-        if($validator->fails()){
-                return response()->json($validator->errors()->toJson(),400);
-        }
-        
-            try {
-                $misReservas = User::with('misReservas')->where('id_usuario',$id_usuario)->first();
-            } catch (\Throwable $th) {
-                return response()->json([
-                    "message" => "Error al consultar las reservas",
-                    "error" => $th->getMessage()
-                ],503);
-            }
-    
-            return response()->json($misReservas);
-        
     }
 
-    public function modificarEstadoReserva(){
-        $validator = Validator::make($req->all(), [
-            'id_reserva' => 'required',
-            'estado' => 'required',
+    return response()->json("Oferta actualizada con exito");
+}
+
+public function getOferta($id_oferta){
+    try {
+        $oferta = Oferta::where('id_oferta',$id_oferta)->firstOrFail();
+    } catch (\Throwable $th) {
+        return response()->json([
+            "message" => "Error al consultar la oferta",
+            "error" => $th->getMessage()
+        ],406);
+    }
+
+    return response()->json($oferta);
+}
+
+public function deleteOferta($id_oferta){
+    try {
+        $oferta = Oferta::where('id_oferta',$id_oferta)->firstOrFail();
+    } catch (\Throwable $th) {
+        return response()->json([
+            "message" => "Error al querer eliminar la oferta",
+            "error" => $th->getMessage()
         ]);
-
-        if($validator->fails()){
-            return response()->json($validator->errors()->toJson(),400);
-        }
-
-
-        try {
-            $reserva = Reserva::where('id_reserva',$req->id_reserva)->firstOrFail();
-        } catch (\Throwable $th) {
-            return response()->json([
-                "message" =>"No existe la reserva",
-                "error" => $th->getMessage()
-            ],406);
-        }
-
-        $reserva->estado = $req->estado;
-
-        try {
-            $reserva->save();
-        } catch (\Throwable $th) {
-            return response()->json(
-                [
-                    "message" => "Error al crear la reserva",
-                    "error" => $th->getMessage()
-                ], 503
-            );
-        }
-
-        return response()->json("Estado actualizado");
     }
 
-    public function modificarReserva(Request $req){
-        $validator = Validator::make($req->all(), [
-            'id_reserva' => 'required',
-            'id_pago' => 'required',
-            'estado' => 'required',
-            'id_usuario' => 'required',
+    try {
+        $oferta->delete();
+    } catch (\Throwable $th) {
+        return response()->json([
+            "message" => "Error al eliminar la oferta",
+            "error" => $th->getMessage()
         ]);
-
-        if($validator->fails()){
-                return response()->json($validator->errors()->toJson(),400);
-        }
-
-        try {
-            $reserva = Reserva::where('id_reserva',$req->id_reserva)->firstOrFail();
-        } catch (\Throwable $th) {
-            return response()->json([
-                "message" =>"No existe la reserva",
-                "error" => $th->getMessage()
-            ],406);
-        }
-
-        $reserva->id_pago = $req->id_pago;
-        $reserva->estado = $req->estado;
-        $reserva->id_usuario = $req->id_usuario;
-
-        try {
-            $reserva->save();
-        } catch (\Throwable $th) {
-            return response()->json(
-                [
-                    "message" => "Error al crear la reserva",
-                    "error" => $th->getMessage()
-                ], 503
-            );
-        }
-
-        return response()->json("Reserva modificada");
     }
 
-    public function nuevaReserva(Request $req){
+    return response()->json("Oferta eliminada");
+}
 
-        $validator = Validator::make($req->all(), [
-            'id_pago' => 'required',
-            'estado' => 'required',
-            'id_usuario' => 'required',
+public function getAllOfertas(){
+    try {
+        $ofertas = Oferta::all();
+    } catch (\Throwable $th) {
+        return response()->json([
+            "message" => "Error al consultar todas las ofertas disponibles",
+            "error" => $th->getMessage()
         ]);
-
-        if($validator->fails()){
-                return response()->json($validator->errors()->toJson(),400);
-        }
-
-        $reserva = new Reserva();
-
-        $reserva->id_pago = $req->id_pago;
-        $reserva->estado = $req->estado;
-        $reserva->id_usuario = $req->id_usuario;
-
-        try {
-            $reserva->save();
-        } catch (\Throwable $th) {
-            return response()->json(
-                [
-                    "message" => "Error al crear la reserva",
-                    "error" => $th->getMessage()
-                ], 503
-            );
-        }
-
-        return response()->json("Reserva creada");
     }
 
-    public function nuevoPago(Request $req){
-        $validator = Validator::make($req->all(), [
-            'monto' => 'required',
+    return response()->json($ofertas);
+}
+
+//A partir de acá Vuelo
+public function vuelosPorFecha(Request $req){
+    try {
+        $vuelo['ida'] = Vuelo::with('vueloConAsientos')
+        ->where([
+            ['aeropuertoOrigen',$req->origen],
+            ['aeropuertoDestino',$req->destino]
+        ])
+        ->whereDate('fechaYHoraPartida',$req->fechaPartida)
+        ->get();
+
+        $vuelo['vuelta'] = Vuelo::with('vueloConAsientos')
+        ->where([
+            ['aeropuertoOrigen',$req->destino],
+            ['aeropuertoDestino',$req->origen],
+        ])
+        ->whereDate('fechaYHoraPartida',$req->fechaVuelta)
+        ->get();
+    } catch (\Throwable $th) {
+        return response()->json([
+            "message" => "Error al consultar los vuelos por fecha",
+            "error" => $th->getMessage()
+        ],503);
+    }
+
+    return response()->json($vuelo);
+}
+
+public function createVuelo(Request $req){
+    $vuelo = new Vuelo();
+
+    $vuelo->aeropuertoDestino = $req->id_aeropuertoDestino;
+    $vuelo->aeropuertoOrigen =$req->id_aeropuertoOrigen;
+    $vuelo->fechaYHoraArribo = $req->fechaYHoraArribo;
+    $vuelo->fechaYHoraPartida = $req->fechaYHoraPartida;
+    $vuelo->precio = $req->precio;
+
+    try {
+        $vuelo->save();
+    } catch (\Throwable $th) {
+        return response()->json([
+            "message" => "Error al guardar el vuelo",
+            "error" => $th->getMessage()
         ]);
-
-        if($validator->fails()){
-                return response()->json($validator->errors()->toJson(),400);
-        }
-
-        $pago=new Pago();
-
-        $pago->monto=$req->monto;
-
-        try {
-            $pago->save();
-        } catch (\Throwable $th) {
-            return response()->json([
-                "message" => "Error al efectuar el pago",
-                "error" => $th->getMessage()
-            ],503);
-        }
-
-        return response()->json("Pago efectuado con exito");
     }
+
+    return response()->json("Vuelo creado con exito");
+}
+
+public function updateVuelo(Request $req){
+    try {
+        $vuelo = Vuelo::where('id_vuelo',$req->id_vuelo)->firstOrFail();
+    } catch (\Throwable $th) {
+        return response()->json([
+            "message" => "vuelo seleccionado inexistente",
+            "error" => $th->getMessage()
+        ],406);
+    }
+
+    $vuelo->aeropuertoDestino = $req->id_aeropuertoDestino;
+    $vuelo->aeropuertoOrigen =$req->id_aeropuertoOrigen;
+    $vuelo->fechaYHoraArribo = $req->fechaYHoraArribo;
+    $vuelo->fechaYHoraPartida = $req->fechaYHoraPartida;
+    $vuelo->precio = $req->precio;
+
+    try {
+        $vuelo->save();
+    } catch (\Throwable $th) {
+        return response()->json([
+            "message" => "Error al actualizar el vuelo",
+            "error" => $th->getMessage()
+        ]);
+    }
+
+    return response()->json("vuelo actualizado con exito");
+}
+
+public function getVuelo($id_vuelo){
+    try {
+        $vuelo = Vuelo::with('vueloConAsientos')
+        ->where('id_vuelo',$id_vuelo)
+        ->firstOrFail();
+    } catch (\Throwable $th) {
+        return response()->json([
+            "message" => "Error al consultar el vuelo",
+            "error" => $th->getMessage()
+        ],406);
+    }
+
+    return response()->json($vuelo);
+}
+
+public function deleteVuelo($id_vuelo){
+    try {
+        $vuelo = Vuelo::where('id_vuelo',$id_vuelo)->firstOrFail();
+    } catch (\Throwable $th) {
+        return response()->json([
+            "message" => "Error al querer eliminar el vuelo",
+            "error" => $th->getMessage()
+        ]);
+    }
+
+    try {
+        $vuelo->delete();
+    } catch (\Throwable $th) {
+        return response()->json([
+            "message" => "Error al eliminar el vuelo",
+            "error" => $th->getMessage()
+        ]);
+    }
+
+    return response()->json("vuelo eliminado");
+}
+
+public function getAllVuelos(){
+    try {
+        $vuelos = Vuelo::all();
+    } catch (\Throwable $th) {
+        return response()->json([
+            "message" => "Error al consultar todos los vuelos disponibles",
+            "error" => $th->getMessage()
+        ]);
+    }
+
+    return response()->json($vuelos);
+}
 
 }
--- API/routes/api.php
+++ API/routes/api.php
@@ -2,12 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ServicioController;
-use App\Http\Controllers\OfertaController;
-use App\Http\Controllers\VueloController;
 use App\Http\Controllers\ReservaController;
 use App\Http\Controllers\AeropuertoController;
+use App\Http\Controllers\AvionController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -22,65 +22,72 @@ use App\Http\Controllers\AeropuertoController;
 Route::post('register',[UserController::class, 'register']);
 Route::post('login',[UserController::class, 'authenticate']);
 
+
+ //Reserva controller
+ Route::post('reserva/updateEstado',[ReservaController::class,'modificarEstadoReserva']);
+ Route::post('servicio/new',[ReservaController::class,'agregarServicio']);
+ Route::post('reserva/new',[ReservaController::class,'nuevaReserva']);
+ Route::post('reserva/update',[ReservaController::class,'modificarReserva']);
+ Route::post('pago/new',[ReservaController::class,'nuevoPago']);
+ Route::get('reserva/get/{id_usuario}',[ReservaController::class,'listarReservas']);
+
+ //----------------------------------------------------------------------------------------------------
+
+ //Servicio controller
+
+ //CRUD OFERTA
+ Route::post('oferta/new',[ServicioController::class,'createOferta']);
+ Route::post('oferta/update',[ServicioController::class,'updateOferta']);
+ Route::delete('oferta/delete/{id_oferta}',[ServicioController::class,'deleteOferta']);
+ Route::get('oferta/getById/{id_oferta}',[ServicioController::class,'getOferta']);
+ Route::get('oferta/getAll',[ServicioController::class,'getAllOfertas']);
+
+ //CRUD VUELO
+ Route::post('vuelo/new',[ServicioController::class,'createVuelo']);
+ Route::post('vuelo/update',[ServicioController::class,'updateVuelo']);
+ Route::delete('vuelo/delete/{id_vuelo}',[ServicioController::class,'deleteVuelo']);
+ Route::post('vuelosPorFecha',[ServicioController::class,'vuelosPorFecha']);
+ Route::get('vuelo/getAll',[ServicioController::class,'getAllVuelos']);
+
+ //----------------------------------------------------------------------------------------------------
+ //Aeropuerto Controller
+ //CRUD AEROPUERTO
+ Route::post('aeropuerto/new',[AeropuertoController::class,'create']);
+ Route::post('aeropuerto/update',[AeropuertoController::class,'update']);
+ Route::delete('aeropuerto/delete/{id_aeropuerto}',[AeropuertoController::class,'delete']);
+ Route::get('aeropuerto/getById/{id_aeropuerto}',[AeropuertoController::class,'get']);
+ Route::get('aeropuerto/getAll',[AeropuertoController::class,'getAll']);
+
+ //----------------------------------------------------------------------------------------------------
+ //Avion Controller
+ //CRUD AVION Y ASIENTOS
+ Route::post('avion/new',[AvionController::class,'create']);
+ Route::post('avion/update',[AvionController::class,'update']);
+ Route::delete('avion/delete/{id_avion}',[AvionController::class,'delete']);
+ Route::get('avion/getById/{id_avion}',[AvionController::class,'get']);
+ Route::get('avion/getAll',[AvionController::class,'getAll']);
+
+ Route::post('asiento/new',[AvionController::class,'createAsientos']);
+ Route::post('asiento/tomarAsiento',[AvionController::class,'tomarAsiento']);
+ Route::post('asiento/update',[AvionController::class,'updateAsiento']);
+ Route::delete('asiento/delete/{id_asiento}',[AvionController::class,'deleteAsiento']);
+ Route::get('asiento/getById/{id_asiento}',[AvionController::class,'getAsiento']);
+ Route::get('asiento/asientosLibres/{id_avion}',[AvionController::class,'asientosLibres']);
+ Route::get('asiento/getAll',[AvionController::class,'getAllAsientos']);
+
+
+ Route::post('user',[UserController::class, 'getAuthenticatedUser']);
+
+ Route::post('logout', [UserController::class, 'logout']);
+
 Route::middleware(['jwt.verify'])->group(function () {
 
     Route::middleware(['rol.verify'])->group(function () {
-        Route::post('reserva/updateEstado',[ServicioController::class,'modificarEstadoReserva']);
-        Route::post('servicio/new',[ServicioController::class,'agregarServicio']);
-        Route::post('reserva/new',[ServicioController::class,'nuevaReserva']);
-        Route::post('reserva/update',[ServicioController::class,'modificarReserva']);
-      
-        Route::post('pago/new',[ServicioController::class,'nuevoPago']);
+
     });
 
-   
 });
 
 
 
-Route::get('reserva/get/{id_usuario}',[ServicioController::class,'listarReservas']);
 
-//CRUD OFERTA
-
-Route::post('oferta/new',[OfertaController::class,'create']);
-Route::post('oferta/update',[OfertaController::class,'update']);
-Route::delete('oferta/delete/{id_oferta}',[OfertaController::class,'delete']);
-Route::get('oferta/getById/{id_oferta}',[OfertaController::class,'get']);
-Route::get('oferta/getAll',[OfertaController::class,'getAll']);
-
-//CRUD AEROPUERTO
-
-Route::post('aeropuerto/new',[AeropuertoController::class,'create']);
-Route::post('aeropuerto/update',[AeropuertoController::class,'update']);
-Route::delete('aeropuerto/delete/{id_aeropuerto}',[AeropuertoController::class,'delete']);
-Route::get('aeropuerto/getById/{id_aeropuerto}',[AeropuertoController::class,'get']);
-Route::get('aeropuerto/getAll',[AeropuertoController::class,'getAll']);
-
- //CRUD VUELO
-
- Route::post('vuelo/new',[VueloController::class,'create']);
- Route::post('vuelo/update',[VueloController::class,'update']);
- Route::delete('vuelo/delete/{id_vuelo}',[VueloController::class,'delete']);
- Route::post('vuelosPorFecha',[VueloController::class,'vuelosPorFecha']);
- Route::get('vuelo/getAll',[VueloController::class,'getAll']);
- Route::get('vuelo/getById/{id_vuelo}',[VueloController::class,'get']);
-  //CRUD AVION Y ASIENTOS
-
-  Route::post('avion/new',[AvionController::class,'create']);
-  Route::post('avion/update',[AvionController::class,'update']);
-  Route::delete('avion/delete/{id_avion}',[AvionController::class,'delete']);
-  Route::get('avion/getById/{id_avion}',[AvionController::class,'get']);
-  Route::get('avion/getAll',[AvionController::class,'getAll']);
-
-  Route::post('asiento/new',[AvionController::class,'createAsientos']);
-  Route::post('asiento/tomarAsiento',[AvionController::class,'tomarAsiento']);
-  Route::post('asiento/update',[AvionController::class,'updateAsiento']);
-  Route::delete('asiento/delete/{id_asiento}',[AvionController::class,'deleteAsiento']);
-  Route::get('asiento/getById/{id_asiento}',[AvionController::class,'getAsiento']);
-  Route::get('asiento/asientosLibres/{id_avion}',[AvionController::class,'asientosLibres']);
-  Route::get('asiento/getAll',[AvionController::class,'getAllAsientos']);
-
-
-Route::post('user',[UserController::class, 'getAuthenticatedUser']);
-
-Route::post('logout', [UserController::class, 'logout']);
